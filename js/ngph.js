dfef08ae5a0ae1be1283fb093d697b869ebbda17
Chuck Kennicott <ckennicott@gmail.com>
2015-09-20 20:10:40 -0700

    added stop

--- js/ngph.js
+++ js/ngph.js
@@ -13,7 +13,10 @@ phr.controller("timerController", function($sce, $scope, $timeout){
   $scope.countdown = function() {
     if ($scope.secToGo < 1) {
       $scope.shotToGo -= 1;
-      $scope.secToGo = 60;
+      $scope.secToGo = 61;
+
+
+
       if ((60- $scope.shotToGo) < 30) {
         nextdeg = 90 + (6 * (60 - $scope.shotToGo));
         $scope.shotProgressStyle = {"background-image": "linear-gradient(90deg, #F0F8FF 50%, transparent 50%, transparent), linear-gradient(" + nextdeg + "deg, #6495ED 50%, #F0F8FF 50%, #F0F8FF)"};
@@ -36,12 +39,21 @@ phr.controller("timerController", function($sce, $scope, $timeout){
       nextdeg = -90 + (6 * -($scope.secToGo - 30));
       $scope.secProgressStyle = {"background-image": "linear-gradient(" + nextdeg + "deg, #6495ED 50%, transparent 50%, transparent), linear-gradient(270deg, #6495ED 50%, #F0F8FF 50%, #F0F8FF)"};
     }
-    $scope.timerPromise = $timeout($scope.countdown, 1000);
+
+    if ($scope.shotToGo == 0) {
+      $timeout.cancel(timerPromise);
+    }
+    else {
+      $scope.timerPromise = $timeout($scope.countdown, 10);
+    }
+    
+
+
   }
 
   $scope.start = function() {
     $scope.disableStart = true;
-    $scope.timerPromise = $timeout($scope.countdown, 1000);
+    $scope.timerPromise = $timeout($scope.countdown, 10);
   }
 
   $scope.stop = function() {
@@ -51,7 +63,7 @@ phr.controller("timerController", function($sce, $scope, $timeout){
   $scope.reset = function() {
     $timeout.cancel($scope.timerPromise);
     $scope.secToGo = 60;
-    $scope.shotToGo = 59;
+    $scope.shotToGo = 60;
     $scope.timerPromise = null;
     $scope.disableStart = false;
     $scope.shotProgressStyle = {"background-image": "linear-gradient(90deg, #F0F8FF 50%, transparent 50%, transparent), linear-gradient(90deg, #6495ED 50%, #F0F8FF 50%, #F0F8FF)"};
