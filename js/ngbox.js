803b379de50a200bb0088e56f7ddec558e8f62ba
Chuck Kennicott <ckennicott@gmail.com>
2015-09-24 07:15:38 -0700

    added bp and boxing

---
+++ js/ngbox.js
@@ -0,0 +1,13 @@
+var kc = angular.module("kc", []);
+
+phr.controller("diceController", function($sce, $scope, $timeout){
+  
+
+
+});
+
+phr.controller("timerController", function($sce, $scope, $timeout){
+  
+
+
+});
