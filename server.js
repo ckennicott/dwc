488899e945b20490540605a3736f79cac3a13186
Chuck Kennicott <ckennicott@gmail.com>
2016-01-17 15:23:15 -0800

    updated ignore

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = module.exports = express();
+
+
+app.use(require('prerender-node').set('prerenderServiceUrl', 'http://localhost:3000'));
+app.use(express.static("."));
+
+app.get('*', function(req, res){ 
+  res.sendFile(__dirname + '/index.html'); 
+});
+
+app.listen(8081); 
+console.log("Go Prerender Go!");
