5edc7ab26e427b1b91f48d7c200842c6a1f5833e
Chuck Kennicott <ckennicott@gmail.com>
2016-01-17 14:01:40 -0800

    added prerender and express

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(require('prerender-node').set('prerenderServiceUrl', 'http://localhost:3
 app.use(express.static("."));
 
 app.get('*', function(req, res){ 
-  res.sendFile(__dirname + './index.html'); 
+  res.sendFile(__dirname + '/index.html'); 
 });
 
 app.listen(8081); 
